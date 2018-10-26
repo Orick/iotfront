fbe6ac66a8d4ff53cb6f6d46a89255f6620bc6c1
jcarrionramos <pollo_carrion@hotmail.com>
2018-10-25 22:58:17 -0300

    Route Added

--- App.js
+++ App.js
@@ -1,59 +1,13 @@
-import React, {Component} from 'react';
-import {StyleSheet} from 'react-native';
-import Login from './components/login';
-import Main from './components/main';
-import recuperarContrasena from './components/recuperarContrasena';
-import crearUsuario from './components/crearUsuario';
+import React from 'react';
+import { Text, View } from 'react-native';
+import { createBottomTabNavigator } from 'react-navigation';
 
+import MapScreen from './src/screens/MapScreen';
+import FriendScreen from './src/screens/FriendScreen';
+import SettingScreen from './src/screens/SettingScreen';
 
-
-import { createStackNavigator } from 'react-navigation';
-
-// const instructions = Platform.select({
-//   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-//   android:
-//     'Double tap R on your keyboard to reload,\n' +
-//     'Shake or press menu button for dev menu TEST'+
-//     '22222',
-// });
-
-const Navigate = createStackNavigator(
-  {
-    Login,
-    Main,
-    recuperarContrasena,
-    crearUsuario
-  },
-  {
-    initialRouteName: 'Login',
-    headerMode: 'none'
-  }
-);
-
-export default class App extends Component {
-  render() {
-    return (
-        <Navigate/>
-    );
-  }
-}
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+export default createBottomTabNavigator({
+    Mapa: MapScreen,
+    Amigos: FriendScreen,
+    Ajustes: SettingsScreen,
 });
---
+++ src/screens/FriendScreen.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class FriendScreen extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Amigos!</Text>
+      </View>
+    );
+  }
+}
+
+export default FriendScreen;
